9763bbcab6e7cd96e784ed46585b0ffac3a186a6
Tomi0928 <tomi09282@gmail.com>
2024-11-22 13:48:53 +0100

    kesz

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -2,14 +2,14 @@
 Console.WriteLine("Hello, World!");
 
 
-public class Matrix<T> where T : struct, IComparable, IConvertible, IFormattable
+public class Matrix<T>
 {
     private T[,] _data;
 
-    public int Rows => _data.GetLength(0); // Sorok száma
-    public int Columns => _data.GetLength(1); // Oszlopok száma
+    public int Rows => _data.GetLength(0); 
+    public int Columns => _data.GetLength(1);
 
-    // Konstruktor: m*n
+
     public Matrix(int rows, int columns)
     {
         if (rows <= 0 || columns <= 0)
@@ -17,17 +17,16 @@ public class Matrix<T> where T : struct, IComparable, IConvertible, IFormattable
         _data = new T[rows, columns];
     }
 
-    // Konstruktor: n*x
+
     public Matrix(int rows) : this(rows, rows)
     {
     }
 
-    // Konstruktor: 3x3 alapértelmezett
     public Matrix() : this(3, 3)
     {
     }
 
-    // Fill metódus
+ 
     public void Fill(T element)
     {
         for (int i = 0; i < Rows; i++)
@@ -35,13 +34,12 @@ public class Matrix<T> where T : struct, IComparable, IConvertible, IFormattable
                 _data[i, j] = element;
     }
 
-    // Mátrix méret visszaadása
     public (int columns, int rows) GetSize()
     {
         return (Columns, Rows);
     }
 
-    // ToString() felülírás
+ 
     public override string ToString()
     {
         var result = "";
@@ -57,7 +55,6 @@ public class Matrix<T> where T : struct, IComparable, IConvertible, IFormattable
         return result;
     }
 
-    // Mátrix összeadása
     public static Matrix<T> Add(Matrix<T> a, Matrix<T> b)
     {
         if (a.Rows != b.Rows || a.Columns != b.Columns)
@@ -70,35 +67,7 @@ public class Matrix<T> where T : struct, IComparable, IConvertible, IFormattable
             {
                 dynamic x = a._data[i, j];
                 dynamic y = b._data[i, j];
-                result._data[i, j] = x + y; // Dinamikus típusok összeadása
-            }
-        }
-        return result;
-    }
-
-    // RotateX: mátrix forgatása X tengely körül
-    public Matrix<T> RotateX()
-    {
-        var result = new Matrix<T>(Rows, Columns);
-        for (int i = 0; i < Rows; i++)
-        {
-            for (int j = 0; j < Columns; j++)
-            {
-                result._data[Rows - 1 - i, j] = _data[i, j];
-            }
-        }
-        return result;
-    }
-
-    // RotateY: mátrix forgatása Y tengely körül
-    public Matrix<T> RotateY()
-    {
-        var result = new Matrix<T>(Rows, Columns);
-        for (int i = 0; i < Rows; i++)
-        {
-            for (int j = 0; j < Columns; j++)
-            {
-                result._data[i, Columns - 1 - j] = _data[i, j];
+                result._data[i, j] = x + y; 
             }
         }
         return result;
